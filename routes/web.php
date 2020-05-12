f27e4911250aaf71a90a513b68220daf69ce3198
showmandas <showmandas3216@gmail.com>
2020-05-12 12:35:58 +0600

    committed

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('crudpage','crudController@index');
-Route::post('store','crudController@store');
-
-Route::post('/edit/{id}','crudController@update');
-Route::post('/delete/{id}','crudController@delete');
+Route::resource('crudpage','crudController');
 
 
