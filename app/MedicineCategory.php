8c66877e3cc17b2e19c984be6e3588624418fdf0
showmandas <showmandas3216@gmail.com>
2020-05-20 10:29:00 +0600

    change

--- app/MedicineCategory.php
+++ app/MedicineCategory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class MedicineCategory extends Model
 {
     protected $table='Medicine_categories';
-    protected $fillable=['name','email','message'];
+    protected $fillable=['name','email','message','phone'];
     public $timestamps=false;
     public  function medichines(){
         return $this->hasMany('App\medichine');
