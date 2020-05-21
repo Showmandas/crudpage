7d416aaaaf5734b7d62cec023de00c0cb8a3f2a0
showmandas <showmandas3216@gmail.com>
2020-05-21 18:24:11 +0600

    filelink

--- resources/views/layout/layouts.blade.php
+++ resources/views/layout/layouts.blade.php
@@ -4,18 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Third</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://kit.fontawesome.com/8143421247.js" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/bootstrap.css')}}">
+   <link rel="stylesheet" href="{{asset('/js/fontawesome-free/css/all.min.css')}}">
+   <link href="{{asset('files/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+
 </head>
 <body>
     <div>
     @yield('content')
     </div>
     <input type="text"  id="indexLink" value="{{route('index')}}" hidden >
-  
+
+    <script src="{{asset('files/jquery/jquery.min.js')}}"></script>
+
+
+    <script src="{{asset('files/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+
+
+    <script src="{{asset('/js/jquery.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="{{asset('/js/bootstrap.js')}}"></script>
+
+  <script src="{{asset('/js/bootstrap.min.js')}}"></script>  
 </body>
 </html>
